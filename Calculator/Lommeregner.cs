0862876e6513cac595f33498788c6f48f1015df9
Daniel Cornelsen <dani179b@edu.eal.dk>
2018-01-24 09:21:22 +0100

    did tings

--- Calculator/Lommeregner.cs
+++ Calculator/Lommeregner.cs
@@ -4,8 +4,9 @@ namespace Calculator
 {
     public class Lommeregner
     {
-        public delegate double Dg_Operation(double p1, double p2);
-        public double Calculate(double p1, double p2, Dg_Operation)
+        public delegate double DgOperation(double p1, double p2);
+
+        public double Calculate(double p1, double p2, 
         {
             return 0.0;
         }
